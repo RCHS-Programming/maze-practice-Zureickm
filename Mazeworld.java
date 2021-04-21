6c263352aad51c7b7bf7f67aa969a81d5fe91a91
Zureick <zureickm@gmail.com>
2021-04-21 08:39:47 -0400

    2

---
+++ Mazeworld.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mazeworld here.
+ * 
+ * @Matthew Zureick (your name) 
+ * @version (a version number or a date)
+ */
+public class Mazeworld extends World
+{
+
+    /**
+     * Constructor for objects of class Mazeworld.
+     * 
+     */
+    public Mazeworld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        for(int i = 0; i < 16; i++)
+        {
+            addObject( new wall("edge.png"), 25 + 50*i,12);
+        }
+        for(int i = 0; i < 16; i++)
+        {
+            addObject( new wall("edge.png"), 25 + 50*i, 587);
+        }
+    }
+}
---
+++ wall.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class wall here.
+ * 
+ * @Matthew Zureick (your name) 
+ * @version (a version number or a date)
+ */
+public class wall extends Actor
+{
+    private String image;
+    public wall( String imageName )
+    {
+        image = imageName;
+        setImage( image );
+        
+    }
+    /**
+     * Act - do whatever the wall wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
