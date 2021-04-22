25bf386bf8f90533d8e5807aafff737c1e6b00e3
Zureick <zureickm@gmail.com>
2021-04-22 08:00:21 -0400

    done ig

--- Mazeworld.java
+++ Mazeworld.java
@@ -25,5 +25,20 @@ public class Mazeworld extends World
         {
             addObject( new wall("edge.png"), 25 + 50*i, 587);
         }
+         for(int i = 0; i < 16; i++)
+        {
+            addObject( new wall("edge2.png"), 12, 25 + 50*i);
+        }
+          for(int i = 0; i < 16; i++)
+        {
+            addObject( new wall("edge2.png"), 788, 25 + 50*i);
+        }
+          for(int i = 0; i < 7; i++)
+        {
+            addObject( new wall("edge.png"), 25 + 50*i, 150);
+        }  for(int i = 0; i < 7; i++)
+        {
+            addObject( new wall("edge2.png"), 125, 300 + 50*i);
+        }
     }
 }
